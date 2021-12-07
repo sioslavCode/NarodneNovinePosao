d070eca7ec75a8de45fdd6c401d748452d1fe27b
Komp <sioslav.code@gmail.com>
2021-12-07 19:10:03 +0100

    Jos malo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 from email.message import EmailMessage
 from random import randint
+from datetime import date
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,7 +14,7 @@ import smtplib
 # Ovaj broj=131 je tjedan ili tako nekakva objava i po tomo se zapravo može iterirat. A ovo rpp je broj redaka pa da nemoramo pagination staviomo 200
 from selenium.webdriver.common.by import By
 
-url = "https://narodne-novine.nn.hr/search.aspx?sortiraj=4&kategorija=3&ogvrsta=26&godina=2021&broj=131&rpp=200&qtype=1&pretraga=da"
+url = "https://narodne-novine.nn.hr/search.aspx?sortiraj=4&kategorija=3&ogvrsta=26&godina=2023&broj=131&rpp=200&qtype=1&pretraga=da"
 service = Service("C:/Users/Komp/PycharmProjects/NarodneNovinePosao/chromedriver.exe")
 chrome_options = Options()
 
@@ -23,6 +24,7 @@ driver = webdriver.Chrome(
 )
 lista_objava = []
 objave_s_kljucnim_rijecima = []
+poruka = ""
 
 
 class objava:
@@ -31,15 +33,32 @@ class objava:
         self.link = link
 
 
+# Prvo odi na stranicu pa joj uzmi zadnji NN broj i onda taj broj stavi u URL kako bi se nize izlistale stvari
 driver.get(url)
 print("Otvorio stranicu")
 time.sleep(3)
+
+listaNN = []
+tablicaObjava = driver.find_element(By.ID, "space-table-placeholder")
+zadnji_redak = tablicaObjava.find_elements(By.TAG_NAME, "tr")[-1]
+nnBroj = zadnji_redak.find_elements(By.TAG_NAME, "td")[0].text
+poruka = zadnji_redak.find_elements(By.TAG_NAME, "td")[1].text
+print(poruka)
+
+godina = date.today().year
+
+print(godina)
+print(nnBroj)
 
 # Natječaji su nam već ispisani sada treba kliknuti na svaki. Možemo prvo naslove i linkove staviti u listu ili dict
 
 # Lista evih div elementa koji su sebi sadrze naslov i link
 
-poruka = ""
+url = "https://narodne-novine.nn.hr/search.aspx?sortiraj=4&kategorija=3&ogvrsta=26&godina=" + str(
+    godina) + "&broj=" + str(nnBroj) + "&rpp=200&qtype=1&pretraga=da"
+driver.get(url)
+time.sleep(5)
+
 try:
     div_element_list = driver.find_elements(By.CLASS_NAME, "resultTitle")
 except NoSuchElementException:
@@ -57,7 +76,7 @@ for element in div_element_list:
 
 for jedna_objava in lista_objava:
     # if lista_objava.index(jedna_objava) == 8: break #Ovo sluzi smao za tesitanje tj da prestane radit nakon 5 puta
-    time.sleep(randint(10, 15))
+    time.sleep(randint(7, 12))
     driver.get(jedna_objava.link)
     print(f"Otvaram: {jedna_objava.link}")
     # preuzmi sadzraj tog div elemnta
@@ -93,7 +112,8 @@ msg["To"] = EMAIL_ADRESS
 msg.set_content(poruka)
 
 msg.add_alternative(
-    "<html><body><h1>Narodne Novine posao objava</h1></body>" + poruka +
+    "<html><body><h3>Narodne Novine posao objava</h3>" + "Broj NN: " + nnBroj + "<br>" + poruka +
+    "</body>"
     "</html>",
     subtype="html")
 
